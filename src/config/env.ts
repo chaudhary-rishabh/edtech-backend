fe44b091c41be6f2bc0da9caad84aafc1cc61bc9
chaudhary-rishabh <chaudharyrishabh029@gmail.com>
2025-09-01 22:06:46 +0530

    fixed .env variables types issue

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
 
+
 export const config = {
-    PORT: process.env.PORT || '5000',
-    MONGODB_URI: process.env.MONGODB_URI || '',
-    JWT_SECRET: process.env.JWT_SECRET || '',
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '7d',
-    NODE_ENV: process.env.NODE_ENV || 'development'
+    PORT: process.env.PORT,
+    MONGODB_URI: process.env.MONGODB_URI as string,
+    JWT_SECRET: process.env.JWT_SECRET as string,
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+    NODE_ENV: process.env.NODE_ENV
 } as const;
 
 export type Config = typeof config;
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,13 +1,19 @@
-import jwt from 'jsonwebtoken';
-import { config } from '../config/env.js';
+import jwt from "jsonwebtoken";
+import { config } from "../config/env.js";
 
 export const generateToken = (payload: object): string => {
+    if (!config.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in environment variables");
+    }
     //@ts-ignore
     return jwt.sign(payload, config.JWT_SECRET, {
-        expiresIn: config.JWT_EXPIRES_IN
+        expiresIn: "1d",
     });
 };
 
 export const verifyToken = (token: string): any => {
+    if (!config.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in environment variables");
+    }
     return jwt.verify(token, config.JWT_SECRET);
 };
