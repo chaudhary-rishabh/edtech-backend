1bd831ef7c41c0387782ffcf1c56b3ea7badde8c
chaudhary-rishabh <chaudharyrishabh029@gmail.com>
2025-09-01 17:02:58 +0530

    ent.ts updated dynamic keys

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,13 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 export const config = {
-    PORT:'5000',
-    MONGODB_URI: '',
-    JWT_SECRET: '#',
-    JWT_EXPIRES_IN: '7d',
-    NODE_ENV: 'development'
+    PORT: process.env.PORT || '5000',
+    MONGODB_URI: process.env.MONGODB_URI || '',
+    JWT_SECRET: process.env.JWT_SECRET || '',
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '7d',
+    NODE_ENV: process.env.NODE_ENV || 'development'
 } as const;
 
 export type Config = typeof config;
